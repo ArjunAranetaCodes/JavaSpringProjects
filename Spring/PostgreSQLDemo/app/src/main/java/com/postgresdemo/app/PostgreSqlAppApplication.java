fb74fa9576dd13126cad7910bcf8b11c73fd46b6
Arjun Araneta <arjunaraneta@gmail.com>
2025-01-04 15:06:40 +1100

    Create PostgreSqlAppApplication.java

---
+++ Spring/PostgreSQLDemo/app/src/main/java/com/postgresdemo/app/PostgreSqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.postgresdemo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlAppApplication.class, args);
+	}
+
+}
